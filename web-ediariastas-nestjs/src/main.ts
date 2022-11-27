d6813fd736c58508d1ec5f239d216e9f95ef9cd2
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-11-27 15:00:20 -0300

    config ValidationPipes

--- web-ediariastas-nestjs/src/main.ts
+++ web-ediariastas-nestjs/src/main.ts
@@ -4,10 +4,12 @@ import { join } from 'path';
 import * as methodOverride from 'method-override';
 import * as exphbs from 'express-handlebars';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const viewPath = join(__dirname, '..', 'views');
+  app.useGlobalPipes(new ValidationPipe());
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(viewPath);
