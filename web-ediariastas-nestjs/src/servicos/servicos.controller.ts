1c9a7d5eaf3a68b7f837127b417be7880bd57831
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-11-26 23:39:11 -0300

    Create view & form to servico

--- web-ediariastas-nestjs/src/servicos/servicos.controller.ts
+++ web-ediariastas-nestjs/src/servicos/servicos.controller.ts
@@ -6,24 +6,31 @@ import {
   Patch,
   Param,
   Delete,
+  Render,
 } from '@nestjs/common';
 import { ServicosService } from './servicos.service';
 import { CreateServicoDto } from './dto/create-servico.dto';
 import { UpdateServicoDto } from './dto/update-servico.dto';
 
-@Controller('servicos')
+@Controller('admin/servicos')
 export class ServicosController {
   constructor(private readonly servicosService: ServicosService) {}
+
+  @Get()
+  @Render('servicos/cadastrar')
+  exibirCadastrar() {
+    // return 'Página de cadastro de serviços';
+  }
 
   @Post()
   create(@Body() createServicoDto: CreateServicoDto) {
     return this.servicosService.create(createServicoDto);
   }
 
-  @Get()
-  findAll() {
-    return this.servicosService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.servicosService.findAll();
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
