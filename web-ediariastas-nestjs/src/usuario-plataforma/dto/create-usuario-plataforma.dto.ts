d50fabaee4a4731d50f6fe07901d2f6226f62ba4
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-12-03 00:56:41 -0300

    Create DTO & Validation Usuario

--- web-ediariastas-nestjs/src/usuario-plataforma/dto/create-usuario-plataforma.dto.ts
+++ web-ediariastas-nestjs/src/usuario-plataforma/dto/create-usuario-plataforma.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUsuarioPlataformaDto {}
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateUsuarioPlataformaDto {
+  @IsNotEmpty({ message: 'Campo nome não pode ser vazio' })
+  @Length(3, 255, {
+    message: 'Campo nome deve ter entre 3 a 255 caracteres',
+  })
+  nome: string;
+
+  @IsNotEmpty({ message: 'Campo email não pode ser vazio' })
+  @Length(3, 255, {
+    message: 'Campo email deve ter entre 3 a 255 caracteres',
+  })
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty({ message: 'Campo senha não pode ser vazio' })
+  @Length(8, 20, {
+    message: 'Campo senha deve ter entre 8 a 20 caracters',
+  })
+  password: string;
+
+  @IsNotEmpty({ message: 'Campo senha não pode ser vazio' })
+  @Length(8, 20, {
+    message: 'Campo senha deve ter entre 8 a 20 caracters',
+  })
+  passwordConfirmation: string;
+}
