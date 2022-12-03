8b45357cd5fe2b5cf6ebd13bd0040a6533ef652e
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-12-02 22:02:12 -0300

    Create Usuario Entity

--- web-ediariastas-nestjs/src/usuario-plataforma/entities/usuario-plataforma.entity.ts
+++ web-ediariastas-nestjs/src/usuario-plataforma/entities/usuario-plataforma.entity.ts
@@ -1,1 +1,35 @@
-export class UsuarioPlataforma {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class UsuarioPlataforma {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updateAt: Date;
+}
