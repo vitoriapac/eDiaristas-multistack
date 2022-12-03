687ada48bb40ff12045c7f29c2c26f1d287ed8b9
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-12-03 01:42:31 -0300

    Config Module Usuario

--- web-ediariastas-nestjs/src/usuario-plataforma/usuario-plataforma.module.ts
+++ web-ediariastas-nestjs/src/usuario-plataforma/usuario-plataforma.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsuarioPlataformaService } from './usuario-plataforma.service';
 import { UsuarioPlataformaController } from './usuario-plataforma.controller';
+import { UsuarioPlataforma } from './entities/usuario-plataforma.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsuarioPlataforma])],
   controllers: [UsuarioPlataformaController],
-  providers: [UsuarioPlataformaService]
+  providers: [UsuarioPlataformaService],
 })
 export class UsuarioPlataformaModule {}
