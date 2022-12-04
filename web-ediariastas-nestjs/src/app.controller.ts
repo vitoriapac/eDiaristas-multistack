b21a3e3a3f89552d50f5b706cf5af3f00620cd2c
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-12-04 02:26:52 -0300

    create route login

--- web-ediariastas-nestjs/src/app.controller.ts
+++ web-ediariastas-nestjs/src/app.controller.ts
@@ -5,9 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('home')
-  getHello() {
-    // return this.appService.getHello();
+  @Get('admin/login')
+  @Render('login')
+  getLogin() {
+    return {
+      layout: false,
+    };
   }
 }
