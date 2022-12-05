b9788655c51e0382cd724a696998b563b2147a7b
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-12-04 23:17:55 -0300

    Create guards

--- web-ediariastas-nestjs/src/auth/auth.module.ts
+++ web-ediariastas-nestjs/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
--- web-ediariastas-nestjs/src/auth/session.serializer.ts
+++ web-ediariastas-nestjs/src/auth/session.serializer.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { PassportSerializer } from '@nestjs/passport';
 
+@Injectable()
 export class SessionSerializer extends PassportSerializer {
   serializeUser(user: any, done: (err: Error, user: any) => void): void {
     done(null, user);
