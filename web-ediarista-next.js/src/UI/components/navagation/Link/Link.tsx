14756ea22643aad1bb8109f32318be3018c55468
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-12-27 22:07:59 -0300

    Create Link Component

---
+++ web-ediarista-next.js/src/UI/components/navagation/Link/Link.tsx
@@ -0,0 +1,40 @@
+import NextLink, { LinkProps as NextLinkProps } from "next/link";
+import Router from "next/router";
+import {
+  Link as MuiLink,
+  LinkProps as MuiLinkProps,
+  ButtonProps,
+} from "@mui/material";
+import React, { PropsWithChildren } from "react";
+
+interface LinkProps {
+  href: string;
+  mui?: MuiLinkProps | ButtonProps;
+  next?: NextLinkProps;
+  Component?: React.ElementType;
+}
+
+const Link: React.FC<PropsWithChildren<LinkProps>> = ({
+  children,
+  href,
+  mui,
+  next,
+  Component = MuiLink,
+  ...props
+}) => {
+  const isNextEnv = Boolean(Router.router);
+
+  return isNextEnv ? (
+    <NextLink href={href} passHref {...next}>
+      <Component {...mui} {...props}>
+        {children}
+      </Component>
+    </NextLink>
+  ) : (
+    <Component href={href} {...mui} {...props}>
+      {children}
+    </Component>
+  );
+};
+
+export default Link;
