4a68dadb0da0c70c5502f8a6b0f16291d412a0ae
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-12-29 23:47:49 -0300

    Created social media container in footer

--- web-ediarista-next.js/src/UI/components/surfaces/Footer/Footer.styled.tsx
+++ web-ediarista-next.js/src/UI/components/surfaces/Footer/Footer.styled.tsx
@@ -1,4 +1,12 @@
-import { Container, ListItem, Typography } from "@mui/material";
+import {
+  Container,
+  IconButton,
+  Link,
+  LinkProps,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { PropsWithChildren } from "react";
 
@@ -24,3 +32,50 @@ export const FooterTitle = styled((props: PropsWithChildren) => (
 export const FooterListItem = styled((props: PropsWithChildren) => (
   <ListItem disableGutters {...props} />
 ))``;
+
+export const SocialContainer = styled("div")`
+  display: flex;
+  flex-direction: column;
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    width: 100%;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+  }
+
+  ${({ theme }) => theme.breakpoints.down(726)} {
+    flex-direction: column;
+  }
+`;
+
+export const AppList = styled("ul")`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing()};
+  list-style: none;
+  padding: 0;
+  margin: ${({ theme }) => theme.spacing(2, 0, 3)};
+
+  img {
+    width: 112px;
+  }
+`;
+
+export const SocialButton = styled((props: LinkProps<typeof IconButton>) => (
+  <Link
+    component={IconButton}
+    target={"_blank"}
+    rel="noreferrer noopener"
+    {...props}
+  />
+))`
+  background-color: ${({ theme }) => theme.palette.primary.dark};
+  i {
+    color: ${({ theme }) => theme.palette.primary.contrastText};
+  }
+`;
+
+export const FooterSocialList = styled(List)`
+  display: grid;
+  grid-template-columns: repeat(3, 58px);
+  grid-gap: ${({ theme }) => theme.spacing(1.5)};
+`;
--- web-ediarista-next.js/src/UI/components/surfaces/Footer/Footer.tsx
+++ web-ediarista-next.js/src/UI/components/surfaces/Footer/Footer.tsx
@@ -1,9 +1,13 @@
 import { Box, List, Typography } from "@mui/material";
 import {
+  AppList,
   FooterContainer,
   FooterTitle,
   FooterGrid,
   FooterListItem,
+  SocialContainer,
+  FooterSocialList,
+  SocialButton,
 } from "./Footer.styled";
 import Link from "UI/components/navigation/Link/Link";
 
@@ -53,7 +57,54 @@ const Footer: React.FC = () => {
           </Typography>
         </Box>
 
-        <FooterTitle>Baixe nosso aplicativo</FooterTitle>
+        <SocialContainer>
+          <div>
+            <FooterTitle>Baixe nosso aplicativo</FooterTitle>
+            <AppList>
+              <li>
+                <a
+                  href="https://www.apple.com/br/app-store/"
+                  target={"_blank"}
+                  rel="noreferrer noopener"
+                >
+                  <img src="/img/logos/app-store.png" alt="App Store" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://play.google.com/store/apps"
+                  target={"_blank"}
+                  rel="noreferrer noopener"
+                >
+                  <img src="/img/logos/google-play.png" alt="Google Play" />
+                </a>
+              </li>
+            </AppList>
+          </div>
+          <div>
+            <FooterTitle>Redes Sociais</FooterTitle>
+
+            <FooterSocialList>
+              <FooterListItem>
+                <SocialButton href="/">
+                  <i className="twf-facebook-f" />
+                </SocialButton>
+              </FooterListItem>
+
+              <FooterListItem>
+                <SocialButton href="/">
+                  <i className="twf-instagram" />
+                </SocialButton>
+              </FooterListItem>
+
+              <FooterListItem>
+                <SocialButton href="/">
+                  <i className="twf-youtube" />
+                </SocialButton>
+              </FooterListItem>
+            </FooterSocialList>
+          </div>
+        </SocialContainer>
       </FooterGrid>
     </FooterContainer>
   );
