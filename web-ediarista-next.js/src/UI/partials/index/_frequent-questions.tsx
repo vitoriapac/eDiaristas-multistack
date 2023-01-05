b3218fc85afd47f2a5552e0c5894b8b238ec2e5f
Vitoria Pacheco <vitoriac.pac@gmail.com>
2023-01-04 21:19:17 -0300

    Created AccordionStyled Component

--- web-ediarista-next.js/src/UI/partials/index/_frequent-questions.tsx
+++ web-ediarista-next.js/src/UI/partials/index/_frequent-questions.tsx
@@ -1,4 +1,11 @@
 import {
+  AccordionDetails,
+  AccordionSummary,
+  Container,
+  Typography,
+} from "@mui/material";
+import {
+  AccordionStyled,
   SectionContainer,
   SectionSubtitle,
   SectionTitle,
@@ -9,8 +16,19 @@ const FrequentQuestions = () => {
   return (
     <SectionContainer>
       <Wave src="/img/home/waves.svg" />
-      <SectionTitle>Ainda está com dúvidas?</SectionTitle>
-      <SectionSubtitle>Veja abaixo as perguntas frequentes.</SectionSubtitle>
+      <Container>
+        <SectionTitle>Ainda está com dúvidas?</SectionTitle>
+        <SectionSubtitle>Veja abaixo as perguntas frequentes.</SectionSubtitle>
+        <AccordionStyled>
+          <AccordionSummary expandIcon={<i className="twf-minus" />}>
+            <Typography color={"primary"}>Titulo</Typography>
+          </AccordionSummary>
+
+          <AccordionDetails>
+            <Typography>Detalhes</Typography>
+          </AccordionDetails>
+        </AccordionStyled>
+      </Container>
     </SectionContainer>
   );
 };
