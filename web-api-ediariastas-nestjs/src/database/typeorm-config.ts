f278a4e97ce3a9a4a5c26a53c7b9b3e2c564b182
Vitoria Pacheco <vitoriac.pac@gmail.com>
2023-01-07 17:31:09 -0300

    API TypeOrm Config

---
+++ web-api-ediariastas-nestjs/src/database/typeorm-config.ts
@@ -0,0 +1,22 @@
+import { join } from 'path';
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      database: 'ediaristas',
+      username: 'root',
+      password: '56114067',
+      host: 'localhost',
+      port: 3306,
+      synchronize: false,
+      type: 'mysql',
+      entities: [join(__dirname, '..', '**/*entity.{ts,js}')],
+      migrations: [join(__dirname, '..', './database/migrations/*.{ts,js}')],
+      namingStrategy: new SnakeNamingStrategy(),
+    };
+  }
+}
