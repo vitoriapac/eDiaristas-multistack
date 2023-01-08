01eedcbd35f6b20690912ca16b63b6163e7a34cd
Vitoria Pacheco <vitoriac.pac@gmail.com>
2023-01-08 15:13:56 -0300

    API: Config App Module

--- web-api-ediariastas-nestjs/src/app.module.ts
+++ web-api-ediariastas-nestjs/src/app.module.ts
@@ -1,9 +1,19 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfigService } from './database/typeorm-config';
+import { CidadesAtendidasModule } from './api/cidades-atendidas/cidades-atendidas.module';
+import { DiaristasModule } from './api/diaristas/diaristas.module';
+import { UsuariosModule } from './api/usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    UsuariosModule,
+    CidadesAtendidasModule,
+    DiaristasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
