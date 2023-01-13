33ad7d5e39c7ef33e653d65fc51aaa45b3c32364
Vitoria Pacheco <vitoriac.pac@gmail.com>
2023-01-12 22:21:46 -0300

    FRONT: Created PageTitle Component

---
+++ web-ediarista-next.js/src/UI/components/data-display/PageTitle/PageTitle.tsx
@@ -0,0 +1,21 @@
+import {
+  PageSubtitleStyled,
+  PageTitleContainer,
+  PageTitleStyled,
+} from "./PageTitle.styled";
+
+export interface PageTitleProps {
+  title: string;
+  subtitle?: string;
+}
+
+const PageTitle: React.FC<PageTitleProps> = ({ title, subtitle }) => {
+  return (
+    <PageTitleContainer>
+      <PageTitleStyled>{title}</PageTitleStyled>
+      <PageSubtitleStyled>{subtitle}</PageSubtitleStyled>
+    </PageTitleContainer>
+  );
+};
+
+export default PageTitle;
