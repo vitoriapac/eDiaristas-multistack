470ae41e80bbe36c55468d3e7a1916e9369db22c
Vitoria Pacheco <vitoriac.pac@gmail.com>
2023-01-13 23:05:49 -0300

    FRONT: Created UserInformation Component

---
+++ web-ediarista-next.js/src/UI/components/data-display/UserInformation/UserInformation.tsx
@@ -0,0 +1,38 @@
+import { SystemProps } from "@mui/system";
+import React, { PropsWithChildren } from "react";
+import {
+  AvatarStyled,
+  RatingStyed,
+  UserDescription,
+  UserInformationContainer,
+  UserName,
+} from "./UserInformation.styled";
+
+export interface UserInformationProps {
+  name: string;
+  picture: string;
+  rating: number;
+  description?: string;
+  isRating?: boolean;
+  sx?: SystemProps;
+}
+
+const UserInformation: React.FC<PropsWithChildren<UserInformationProps>> = ({
+  sx,
+  picture,
+  name,
+  rating,
+  description,
+  isRating,
+}) => {
+  return (
+    <UserInformationContainer sx={sx} isRating={isRating}>
+      <AvatarStyled src={picture}>{name[0]}</AvatarStyled>
+      <RatingStyed value={rating} readOnly />
+      <UserName>{name}</UserName>
+      <UserDescription>{description}</UserDescription>
+    </UserInformationContainer>
+  );
+};
+
+export default UserInformation;
