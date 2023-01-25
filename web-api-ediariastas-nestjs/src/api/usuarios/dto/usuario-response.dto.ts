ee7caba601ab68d189818370b007909b6306f0d4
Vitoria Pacheco <vitoriac.pac@gmail.com>
2023-01-25 00:16:17 -0300

    API: Created UsuarioResponse DTO

---
+++ web-api-ediariastas-nestjs/src/api/usuarios/dto/usuario-response.dto.ts
@@ -0,0 +1,11 @@
+export class UsuarioResponseDto {
+  id: number;
+  nomeCompleto: string;
+  email: string;
+  tipoUsuario: number;
+  cpf: string;
+  reputacao: number;
+  nascimento: Date;
+  telefone: string;
+  chavePix: string;
+}
