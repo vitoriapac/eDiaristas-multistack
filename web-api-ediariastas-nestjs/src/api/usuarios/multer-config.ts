d474b6efb382b6f234451ca4e30735977c969f61
Vitoria Pacheco <vitoriac.pac@gmail.com>
2023-01-30 01:04:53 -0300

    API: Update Disk Storage images

--- web-api-ediariastas-nestjs/src/api/usuarios/multer-config.ts
+++ web-api-ediariastas-nestjs/src/api/usuarios/multer-config.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const multerConfig = {
   storage: diskStorage({
-    destination: '../../images/docs',
+    destination: './images/docs',
     filename: (req, file, cb) => {
       const fileName =
         path.parse(file.originalname).name.replace(/\s/g, '') + '-' + uuidv4();
